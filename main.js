8ae0e6d1586e7fc3d937cd40a6f189a14191435d
mahailahmed <160250995+mahailahmed@users.noreply.github.com>
2024-03-05 12:16:19 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//full name 
+// let Name:string="manahil"
+// let secondname:string="muhammad"
+// let thirdname:string="ahmed"
+// let mainname:string=Name+secondname+ thirdname
+// console.log(mainname)
+// concatinate with space
+var Name = "manahil";
+var secondname = "ahmed";
+var thirdname = "khan";
+var mainname = Name + " " + secondname + " " + thirdname;
+console.log(mainname);
---
+++ main.ts
@@ -0,0 +1,16 @@
+//full name 
+
+// let Name:string="manahil"
+// let secondname:string="muhammad"
+// let thirdname:string="ahmed"
+// let mainname:string=Name+secondname+ thirdname
+// console.log(mainname)
+
+
+// concatinate with space
+
+let Name:string="manahil"
+let secondname:string="ahmed"
+let thirdname:string="khan"
+let mainname:string=Name+" "+secondname+" "+thirdname
+console.log(mainname)
